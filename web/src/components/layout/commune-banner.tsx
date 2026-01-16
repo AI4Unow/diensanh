84c6a9beda72e83c726a7a1db6e281844683f392
NAD <duc.a.nguyen@gmail.com>
2026-01-16 17:20:47 +0700

    fix: update commune banner to use official government website URL

--- web/src/components/layout/commune-banner.tsx
+++ web/src/components/layout/commune-banner.tsx
@@ -2,7 +2,7 @@ export function CommuneBanner() {
   return (
     <div className="w-full">
       <img
-        src="/images/banner-xa-dien-sanh.jpg"
+        src="https://diensanh.quangtri.gov.vn/documents/790019/0/bannner-xa-dien-sanh.jpg/5fa0fc6f-0584-ec8f-ee7f-ebfa51005a1c?t=1751078390283"
         alt="UBND Xã Diễn Sanh - Huyện Hải Lăng - Tỉnh Quảng Trị"
         className="w-full h-auto object-cover"
         loading="eager"
